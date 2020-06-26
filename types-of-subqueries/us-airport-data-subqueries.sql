7abcfa9b23a9b787286433dece0e30bbec80e20a
jwelly <64269613+jwelly@users.noreply.github.com>
2020-06-26 16:59:39 +0100

    quick update

--- types-of-subqueries/us-airport-data-subqueries.sql
+++ types-of-subqueries/us-airport-data-subqueries.sql
@@ -45,5 +45,28 @@ SELECT a.dep_month,
  -- In a correlated subquery, the subquery CANNOT be run indepedently of the outer query
  -- 	1) A row is processed in the outer query
  --		2) Then, for that particular row in the outer q, the subquery is executed
+ -- Here, we find the list of all flights whose distance is above average for their carrier.
+ -- The inner query has to be reexecuted for EACH flight.
+SELECT id
+FROM flights AS f
+WHERE distance > (
+  SELECT AVG(distance)
+  FROM flights
+  WHERE carrier = f.carrier);
  
  
+ -- EX 6 correlated subquery (not a good example)
+ -- Here, we give flights a SEQUENCE number, based on ID, carrier
+ -- We assume flight_id increments with each additional flight
+ -- So, all flights with seq no. 1 will be the flight with the lowest ID number of each carrier
+ -- Imagine 2 was the lowest flight no., f.id < 2 would be null, so the count would be 0 + 1 = 1.
+ SELECT
+    carrier,
+    id,
+    (SELECT COUNT(*) + 1
+FROM flights f
+WHERE
+    f.id < flights.id AND
+    f.carrier = flights.carrier) AS flight_sequence_number
+FROM flights;
+
